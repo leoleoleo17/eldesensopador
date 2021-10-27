072842593db64bb3646fdfc4e2009ced6f8ab7ec
leoleoleo17 <67807484+leoleoleo17@users.noreply.github.com>
2021-10-27 09:00:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import funciones
+import GUI2
 
 row1 = "BQWQDMF" 
 row2 = "GKZRUBZ"
@@ -8,10 +9,13 @@ row5 = "RCDSELA"
 row6 = "ZODAFJT"
 row7 = "JUANCHO"
 sopa = (row1, row2, row3, row4, row5, row6, row7)
+coordenadas_dict = {}
 
 words = ("CASA", "JUANCHO", "ZAPATO", "DULCE")
 for x in words:
     coordenadas = ()
-    coordenadas = funciones.buscapalabras(x, sopa) #Ejecuta las funciones que buscan la palabra
+    coordenadas = funciones.buscapalabras(x, sopa)
+    coordenadas_dict[x]= coordenadas
     print("La palabra {0} se encuentra en: ".format(x), coordenadas)
-                
+    
+GUI2.main(coordenadas_dict, sopa)
